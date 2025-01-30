660d279c2593800776faadc24196b7f8cafbbfce
Dzmitry Ramantsevich <d.ramantsevich@gmail.com>
2025-01-30 18:02:27 +0300

    implemented basic security

---
+++ src/main/java/com/example/monitorsensors/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.monitorsensors.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN", "VIEWER")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("VIEWER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(HttpMethod.GET, "/api/sensors/**").hasAnyRole("ADMIN", "VIEWER")
+                        .requestMatchers(HttpMethod.POST, "/api/sensors/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/sensors/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/sensors/**").hasRole("ADMIN")
+                        .requestMatchers("/api/sensors/search/name", "/api/sensors/search/model").hasAnyRole("ADMIN", "VIEWER")
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
